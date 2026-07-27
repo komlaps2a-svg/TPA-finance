6abbe89cab601782fe6d25e476aac9fe1f48ed2e
engyourbae <komlaps2a@gmail.com>
2026-07-28 06:29:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,51 @@
-/* sw.js */
-const CACHE_NAME = 'tpa_finance_v1.6';
-const ASSETS_TO_CACHE = [ './', './index.html', './style.css', './main.js', './manifest.json', './icon-192x192.png' ];
+const CACHE_NAME = 'tpa_finance_v1.7';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './main.js',
+    './manifest.json',
+    './icon-192x192.png'
+];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((cacheNames) => {
-        return Promise.all(cacheNames.map((cache) => { if (cache !== CACHE_NAME) return caches.delete(cache); }));
-    }));
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
     self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        fetch(event.request).then((networkResponse) => {
-            if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
-                const responseToCache = networkResponse.clone();
-                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseToCache));
-            }
-            return networkResponse;
-        }).catch(() => caches.match(event.request))
+        fetch(event.request)
+            .then((networkResponse) => {
+                if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+                    const responseToCache = networkResponse.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+                }
+                return networkResponse;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
